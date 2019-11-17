66d7e400b1f8aa67cd280ae9d966dd687d950fcc
jacquiemcb <53495414+jacquiemcb@users.noreply.github.com>
2019-11-17 03:55:19 -0500

    Add files via upload

---
+++ Instructions/Resources/VBA Easy.vb
@@ -0,0 +1,27 @@
+Sub easy():
+
+Dim Total As Double
+
+RowCount = Cells(Rows.Count, "A").End(xlUp).Row
+For i = 2 To RowCount
+
+    If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
+        Total = Total + Cells(i, 7).Value
+
+           ' Print ticker symbol
+        Range("I" & 2 + j).Value = Cells(i, 1).Value
+           ' Print total
+        Range("J" & 2 + j).Value = Total
+           ' Reset Total
+        Total = 0
+           ' Move to next row
+        j = j + 1
+
+    Else
+        Total = Total + Cells(i, 7).Value
+    End If
+
+    Next i
+
+
+End Sub
