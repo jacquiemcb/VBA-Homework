2843fe601fdb3584bece88452bf1c8f86dcc72ea
jacquiemcb <53495414+jacquiemcb@users.noreply.github.com>
2020-01-23 22:24:29 -0500

    update

--- Instructions/Resources/VB_HW.vb
+++ Instructions/Resources/VB_HW.vb
@@ -1,27 +1,37 @@
 Sub ticker():
 
 Dim Total As Double
+Dim Assignment As Worksheet
+Dim J As Integer
+
+For Each Assignment In Worksheets
+J = 0
+Total = 0
+
 
 RowCount = Cells(Rows.Count, "A").End(xlUp).Row
 For i = 2 To RowCount
 
-    If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
-        Total = Total + Cells(i, 7).Value
+    Assignment.Range("I1").Value = "Ticker"
+    Assignment.Range("J1").Value = "Total"
+
+    If Assignment.Cells(i + 1, 1).Value <> Assignment.Cells(i, 1).Value Then
+        Total = Total + Assignment.Cells(i, 7).Value
 
            ' Print ticker symbol
-        Range("I" & 2 + j).Value = Cells(i, 1).Value
+        Assignment.Range("I" & 2 + J).Value = Cells(i, 1).Value
            ' Print total
-        Range("J" & 2 + j).Value = Total
+        Assignment.Range("J" & 2 + J).Value = Total
            ' Reset Total
         Total = 0
            ' Move to next row
-        j = j + 1
+        J = J + 1
 
     Else
-        Total = Total + Cells(i, 7).Value
+        Total = Total + Assignment.Cells(i, 7).Value
     End If
 
     Next i
-
+    Next Assignment
 
 End Sub
